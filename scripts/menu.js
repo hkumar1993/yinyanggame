98d5c31e7295280d17f8044c599f2ca14ae09762
Harsh <harsh@hkumar.me>
2025-04-26 02:58:54 -0700

    tutorial

--- scripts/menu.js
+++ scripts/menu.js
@@ -2,6 +2,30 @@ import { EVENTS } from './constants.js';
 import eventBus from './eventbus.js';
 import soundboard from './soundboard.js';
 import state from './state.js';
+
+const TUTORIAL_SCREENS = [
+    {
+        src: '../img/balance.png',
+        text: 'Keep the world in balance. Rotate the symbol to collect the dark and light energies of the world.',
+    },
+    {
+        src: '../img/dark-over-light-1.png',
+        text: 'Collect dark energy on the dark side. Don\'t let the dark side take over its inner light.',
+    },
+    {
+        src: '../img/light-over-dark-2.png',
+        text: 'Collect light energy on the dark side. Don\'t let the light take over the dark side.',
+    },
+    {
+        src: '../img/light-over-dark-1.png',
+        text: 'Collect light energy on the dark side. Don\'t let the light side take over its inner dark.',
+    },
+    {
+        src: '../img/dark-over-light-2.png',
+        text: 'Collect dark energy on the light side. Don\'t let the dark take over the light side.',
+    },
+]
+
 export default class MainMenu {
     constructor(game) {
         this.soundboard = soundboard;
@@ -9,7 +33,9 @@ export default class MainMenu {
         this.startButton = document.getElementById('start-game');
         this.muteButton = document.getElementById('mute');
         this.resetButton = document.getElementById('reset');
+        this.tutorialButton = document.getElementById('tutorial');
         this.game = game;
+        this.tutorialStep = 0;
 
         this.startButton.addEventListener('click', () => {
             this.startGame();
@@ -22,6 +48,26 @@ export default class MainMenu {
         this.muteButton.addEventListener('click', () => {
             this.toggleMute();
         });
+        this.tutorialButton.addEventListener('click', () => {
+            this.startTutorial();
+        });
+        this.leftTut = document.getElementById('left-tut');
+        this.rightTut = document.getElementById('right-tut');
+        this.closeTut = document.getElementById('close-tut');
+
+        this.leftTut.addEventListener('click', () => {
+            console.log('prev')
+            this.updateTutorialStep(-1);
+        });
+        this.rightTut.addEventListener('click', () => {
+            console.log('next')
+            this.updateTutorialStep(1);
+        });
+        this.closeTut.addEventListener('click', () => {
+            document.getElementById('tutorial-ui').classList.add('hidden');
+        })
+
+
 
         eventBus.subscribe(EVENTS.GAME_PAUSE, () => {
             this.showMenu();
@@ -74,4 +120,35 @@ export default class MainMenu {
         document.getElementById('final-score-time').innerText = state.endTime;
         this.showMenu();
     }
+
+    startTutorial() {
+        const tutorialUI = document.getElementById('tutorial-ui');
+        tutorialUI.classList.remove('hidden');
+        this.tutorialStep = 0;
+        this.updateTutorialStep(0);
+    }
+
+    updateTutorialStep(change) {
+        this.tutorialStep += change;
+        if (this.tutorialStep === 0) {
+            // disable left tut
+            this.leftTut.setAttribute('disabled', true);
+        } else {
+            this.leftTut.removeAttribute('disabled');
+        }
+        if (this.tutorialStep === TUTORIAL_SCREENS.length - 1) {
+            this.rightTut.setAttribute('disabled', true);
+        } else {
+            this.rightTut.removeAttribute('disabled');
+        }
+
+        const screen = TUTORIAL_SCREENS[this.tutorialStep];
+        const img = document.getElementById('tutorial-img');
+        img.src = screen?.src;
+
+        const text = document.getElementById('tutorial-text');
+        text.innerText = screen?.text;
+    }
+
+
 }
